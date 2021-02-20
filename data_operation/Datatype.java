a4eb0a7efcf90785a117b1a12d406ed37f338efd
yang <kiki9510@gmail.com>
2021-02-20 17:32:28 +0900

    first java code.

---
+++ data_operation/Datatype.java
@@ -0,0 +1,14 @@
+public class Datatype{
+	public static void main(String[] args) {
+		System.out.println(6); // 숫자
+		System.out.println("six"); // 문자
+		
+		System.out.println("6"); // 문자
+		
+		System.out.println(6+6); // 더하기
+		System.out.println("6" + "6"); // 문자 결합
+		
+		System.out.println(6*6);
+		System.out.println("1111".length()); 
+	}
+}
